e147f86966c675e29b819f0ac8fb28ff83847fac
wangxigang <wangxigang@rijin.com>
2016-06-07 10:54:03 +0800

    对短信批量发送功能进行封装

--- sms_request.go
+++ sms_request.go
@@ -5,3 +5,10 @@ type SMSRequest struct {
 	Message  string `json:"message,string"`
 	CallBack string `json:"cb,omitempty"`
 }
+
+type BatchSMSRequest struct {
+	MobileList string `json:"mobile_list"`
+	Message    string `json:"message,string"`
+	Time       string `json:"time, string"`
+	CallBack   string `json:"cb,omitempty"`
+}
